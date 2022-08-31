5912bc1b4a40dcbca4acc3186f2866099b46ed71
Brian Carlin <brian@purplejay.io>
2022-08-31 15:40:06 -0400

    updated poetry install method

--- run.sh
+++ run.sh
@@ -71,7 +71,7 @@ function install-python {
 
 function install-poetry {
   if [[ $HOMEBREW_INSTALLED == 0  && $PYTHON_INSTALLED == 0 && $POETRY_INSTALLED == 1 ]]; then
-    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+    curl -sSL https://install.python-poetry.org | python3 -
     POETRY_INSTALLED=$(test -f $HOME/.poetry/bin/poetry;echo $?)
   fi
   if [[ $POETRY_INSTALLED != 0 ]]; then
